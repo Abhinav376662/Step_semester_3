f14c0b34d8a41321f7a34a42386e8c24deb0dc00
Abhinav376662 <abhinavasole2007@gmail.com>
2026-09-11 21:03:29 +0530

    Create Problem4.java

---
+++ Week2 Problem/Problem4.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+
+public class Problem4 {
+
+    static String maskPhoneNumber(String phone) {
+        if (phone.length() != 10) {
+            return "Invalid phone number";
+        }
+
+        for (int i = 0; i < phone.length(); i++) {
+            if (!Character.isDigit(phone.charAt(i))) {
+                return "Invalid phone number";
+            }
+        }
+
+        String lastFourDigits =
+                phone.substring(phone.length() - 4);
+
+        StringBuilder maskedNumber =
+                new StringBuilder("XXXXXX");
+
+        maskedNumber.insert(
+                maskedNumber.length(),
+                "-"
+        );
+
+        maskedNumber.append(lastFourDigits);
+
+        return maskedNumber.toString();
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter 10-digit phone number: ");
+        String phone = scanner.nextLine();
+
+        System.out.println(maskPhoneNumber(phone));
+
+        scanner.close();
+    }
+}
